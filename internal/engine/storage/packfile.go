56f5e6d9b797d38baeb510c2718c77cd2d72ec61
Hello0 <po1ful117@gamil.com>
2025-11-11 15:45:52 +0900

    PackFilerインタフェースのWriteおよびReadメソッドのシグネチャを変更

--- internal/engine/storage/packfile.go
+++ internal/engine/storage/packfile.go
@@ -1,6 +1,6 @@
 package storage
 
 type PackFiler interface {
-	Write(path string, data []byte) (offset int64, size int64, err error)
-	Read(path string, offset int64, size int64) ([]byte, error)
+	Write(data []byte) (int, int64, int64, error)
+	Read(packID int, offset int64, size int64) ([]byte, error)
 }
